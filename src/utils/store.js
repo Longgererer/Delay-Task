7e0b2ae009e79ee74deb8a7b05d6de08e608bd6c
xiaolong Hao <2072451919@qq.com>
2021-10-26 11:54:19 +0800

    删除不需要的代码

--- src/utils/store.js
+++ src/utils/store.js
@@ -7,13 +7,11 @@ export default (function localData () {
       set: (key, val) => { ipcRenderer.send('save-data', key, val) },
       get: (key) => {
         return ipcRenderer.sendSync('get-data', key)
-      },
-      del: (key) => { store.delete(key) }
+      }
     }
   }
   return {
     set: (key, val) => { localStorage.setItem(key, JSON.stringify(val)) },
     get: (key) => JSON.parse(localStorage.getItem(key) || 'false'),
-    del: (key) => { localStorage.removeItem(key) }
   }
 })()
